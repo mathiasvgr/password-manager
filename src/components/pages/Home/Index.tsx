263fa5fb38b2d4d7a31515fe5ca5cfb283b90c0c
mathiasvgr <mathias.vigier@epitech.eu>
2023-01-11 11:11:32 +0100

    [WIP]: starting the navbar with basic layout and debug border / color, added some icons.

--- src/components/pages/Home/Index.tsx
+++ src/components/pages/Home/Index.tsx
@@ -1,12 +1,14 @@
 import { Outlet} from "react-router-dom";
+import { Navbar } from "./Navbar";
+import styles from "@styles/Home/home.module.css"
 
 function Home() {
 
   return (
-    <>
-      HOME
+    <div className={styles.home}>
+      <Navbar/>
       <Outlet />
-    </>
+    </div>
     );
 }
 
