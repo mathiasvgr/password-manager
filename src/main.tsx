6e7f53ef67c72847d2d0213854796212bcddb8ed
mathiasvgr <mathias.vigier@epitech.eu>
2023-01-15 17:05:54 +0100

    FEAT: changin to material ui theme for color and font

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import { RouterProvider } from "react-router-dom";
 import router from "@router/config"
 import { AuthProvider } from "@hooks/provider/AuthProvider";
 import "@styles/main.css";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import { customTheme } from "@styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={customTheme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </AuthProvider>
   </React.StrictMode>
 );
+
+
+
